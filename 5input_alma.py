72dda2b6d4c341b7cfe75c1ebd2d021560b51a07
Aleyna Dikal <aleyynadikal@gmail.com>
2022-05-29 23:12:55 +0300

    input alma

---
+++ 5input_alma.py
@@ -0,0 +1,12 @@
+yas = input("Yasinizi girin:")
+print("Yasiniz",yas)
+
+a = input("a:") #3
+b = input("b:") #4
+c = input("c:") #5
+print("Toplam ",a+b+c) #345 yazilir.
+
+a = int(input("a:")) #3
+b = int(input("b:")) #4
+c = int(input("c:")) #5
+print("Toplam ",a+b+c) #12
