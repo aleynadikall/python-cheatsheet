058585988bc87693640c4d529213bae9c81cf4db
Aleyna Dikal <aleyynadikal@gmail.com>
2022-05-29 23:10:53 +0300

    veri tipleri

---
+++ 2veri_tipleri.py
@@ -0,0 +1,17 @@
+a = 3 #integer veri tipi
+b = 3.14 #float veri tipi
+c = "Python" #string veri tipi
+d = [1,2,3,4,"Python"] #liste veri tipi
+e = (1,2,3,4,"Python") #tuple veri tipi
+f = {"Elma":3,"Armut":4,"Kiraz":5} #sozluk veri tipi
+g = True #boolean veri tipi (True ya da False seklinde degerler alir.)
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+print(d,type(d))
+print(e,type(e))
+print(f,type(f))
+print(g,type(g))
+
+print(a,b,c)
+
