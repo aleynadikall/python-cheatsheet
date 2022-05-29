2e44cbdfbfb8ea001496fc7da81b0811c5f5026e
Aleyna Dikal <aleyynadikal@gmail.com>
2022-05-30 00:04:30 +0300

    For Döngüsü

---
+++ 10for_dongusu.py
@@ -0,0 +1,14 @@
+a = [1, 2, 3, 4, 5, 6]
+b = "Python"
+
+for eleman in a:
+    print(eleman)
+
+for karakter in b:
+    print(karakter)
+
+for sayi in range(0,100):
+    print(sayi)
+
+for sayi in range(20,30,2): # 2'ser 2'ser gider (30 dahil degil)
+    print(sayi)
