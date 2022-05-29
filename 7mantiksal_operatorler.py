66eb8d6543b37efe06a4b3ae31f5e789deb05b5c
Aleyna Dikal <aleyynadikal@gmail.com>
2022-05-29 23:46:22 +0300

    Mantıksal Operatörler

---
+++ 7mantiksal_operatorler.py
@@ -0,0 +1,29 @@
+a = 3
+b = 4
+if a == 3 and b == 4:
+    print("Evet")
+else:
+    print("Hayir")
+
+if a == 3 and b == 5:
+    print("Evet")
+else:
+    print("Hayir")
+
+if a == 3 or b == 4:
+    print("Evet")
+else:
+    print("Hayir"),
+
+if a == 4 or b == 4:
+    print("Evet")
+else:
+    print("Hayir")
+
+if a == 4 or b == 3:
+    print("Evet")
+else:
+    print("Hayir")
+
+if (not(3 == 4)):
+    print("Evet")
