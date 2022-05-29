74d68a8485881981ce11ef7dad236e0001ebeb0b
Aleyna Dikal <aleyynadikal@gmail.com>
2022-05-29 23:11:32 +0300

    matematiksel operatörler

---
+++ 3matematik.py
@@ -0,0 +1,29 @@
+print(3 + 4)
+print(10 - 3)
+print(10 * 3)
+print(10 / 4)
+#bucuklu kismi istemiyorsak;
+print(10 // 4)
+print(10 % 4)
+
+a = 5
+b = 10
+c = a + 2 * b
+print(c)
+
+k = "Python "
+l = "Programlama "
+m= "Dili"
+n = k + l +m
+print(n)
+
+o = "Python "
+print(o*5)
+
+print("* " * 1)
+print("* " * 2)
+print("* " * 3)
+print("* " * 4)
+print("* " * 5)
+print("* " * 6)
+
