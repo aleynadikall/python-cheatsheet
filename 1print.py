0f13cbf8770b986cbfffe93d8436a042618bbb6e
Aleyna Dikal <aleyynadikal@gmail.com>
2022-05-29 23:10:04 +0300

    print

---
+++ 1print.py
@@ -0,0 +1,14 @@
+print("Python")
+print("Naber")
+print("Aleyna",14,"Dikal")
+print("Aleyna\nDikal")
+print("19","09","1993")
+print("19","09","1993",sep="/") #Bu satir ekrana dogum tarihini yazar. (Yorum satiri boyle yazilir.)
+print("{} + {} = {}".format(2,3,2+3))
+
+#Coklu yorum satiri boyle yazilir;
+"""
+1 derste python
+Hadi bakalimmmm.
+
+"""
